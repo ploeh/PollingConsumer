8ba43084685d22b823a1eb3542a657249fabd646
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-07-20 16:42:20 +0300

    Remove redundant semicolon

--- PollingConsumer/ColorPrint.fs
+++ PollingConsumer/ColorPrint.fs
@@ -13,7 +13,7 @@ let cprintf color format =
     let print (s : string) = 
         let old = System.Console.ForegroundColor 
         try 
-            System.Console.ForegroundColor <- color;
+            System.Console.ForegroundColor <- color
             System.Console.Write s
         finally
             System.Console.ForegroundColor <- old
